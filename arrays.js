d0d1b8de8471a896b536c14bc33117dd7e6f6c15
ruan-jac-dev <ruanfjacobs@gmail.com>
2026-04-13 20:21:59 +0200

    Add arrays, functions and objects practice files

---
+++ arrays.js
@@ -0,0 +1,8 @@
+const fruits = ["apple", "banana", "orange"];
+
+console.log(fruits[0]);
+console.log(fruits.length);
+
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
---
+++ objects.js
@@ -0,0 +1,8 @@
+const person = {
+  name: "Ruan",
+  role: "Junior Developer",
+  learning: "JavaScript"
+};
+
+console.log(person.name);
+console.log(person.role);
